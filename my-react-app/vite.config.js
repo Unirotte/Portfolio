0da0206072fb57e30eb111df4e56f8bb4a3e505a
Unirotte <PichenotJudith@live.fr>
2025-10-23 10:37:56 +0200

    fix: inclusion du fichier _redirects dans le build

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+  base: '/', 
   plugins: [react()],
+  build: {
+    assetsDir: "",      
+  },
+  publicDir: "public",  
 })
