fcdbea1abe704b875b1257e7e7b8fc2e813deb15
Unirotte <PichenotJudith@live.fr>
2025-10-23 10:41:19 +0200

    On essaie encore de faire fonctionner Netlify

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/', 
   plugins: [react()],
-  build: {
-    assetsDir: "",      
+  build: {  
   },
   publicDir: "public",  
 })
