a5330d8c5fcb0570c628f8dbbac92f46458be4e6
Unirotte <PichenotJudith@live.fr>
2025-10-23 10:48:42 +0200

    on essaie encore

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/', 
   plugins: [react()],
-  build: {  
+  build: {
   },
   publicDir: "public",  
 })
+// rebuild trigger
