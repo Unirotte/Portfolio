620bf69d9be05cc0cdb85a3badefc3bf2f42b058
Unirotte <PichenotJudith@live.fr>
2025-10-28 12:46:12 +0100

    alors ajout des min width

--- my-react-app/src/components/IndexComponents.jsx
+++ my-react-app/src/components/IndexComponents.jsx
@@ -12,11 +12,11 @@ const AllDraw = lazy(() => import("./AllDraw.jsx"));
 const MiniGallery = lazy(() => import( "./MiniGallery.jsx"));
 const LinkToiles = lazy(() => import("./LinkToiles.jsx"));
 
-import useFadeInOnScroll from "../hooks/HookScrool.jsx";
 import Me from "./Me.jsx";
-import TriggerWarningModal from "./TriggerWarning.jsx";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
+import useFadeInOnScroll from "../hooks/HookScrool.jsx";
+import TriggerWarningModal from "./TriggerWarning.jsx";
 import ScrollToAnchor from "../hooks/SrollToAnchor.jsx";
 
 export { Me,
