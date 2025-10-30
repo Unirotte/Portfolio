d3ce30b22da890be5ce5a4f5d2f986b436d42ee0
Unirotte <PichenotJudith@live.fr>
2025-10-30 17:35:06 +0100

    fix: chemins absolus pour Netlify

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', 
+  base: '/', 
   plugins: [react()],
   build: {
   },
   publicDir: "public",  
 })
-// rebuild trigger
